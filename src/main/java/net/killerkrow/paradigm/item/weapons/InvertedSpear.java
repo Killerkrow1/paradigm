2530e0a217fc1a9774887883ff2c6c638f6a5de0
Killerkrow <cpf283@gmail.com>
2026-06-19 23:37:36 -0500

    Moved the reach attribute api thingie to this branch so that the spear could have increased range

--- src/main/java/net/killerkrow/paradigm/item/weapons/InvertedSpear.java
+++ src/main/java/net/killerkrow/paradigm/item/weapons/InvertedSpear.java
@@ -2,9 +2,11 @@ package net.killerkrow.paradigm.item.weapons;
 
 import com.google.common.collect.ImmutableMultimap;
 import com.google.common.collect.Multimap;
+import com.jamieswhiteshirt.reachentityattributes.ReachEntityAttributes;
 import net.killerkrow.paradigm.util.ModRarities;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.item.TooltipContext;
+import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.attribute.EntityAttribute;
 import net.minecraft.entity.attribute.EntityAttributeModifier;
@@ -20,8 +22,10 @@ import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
+import java.util.UUID;
 
 public class InvertedSpear extends SwordItem implements Vanishable {
+    private static final UUID ATTACK_RANGE_MODIFIER_UUID = UUID.fromString("12345678-1234-5678-1234-567812345678");
     private final float attackDamage;
     private final Multimap<EntityAttribute, EntityAttributeModifier> attributeModifiers;
     private final ModRarities rarity;
@@ -51,6 +55,28 @@ public class InvertedSpear extends SwordItem implements Vanishable {
     @Override
     public ItemStack getRecipeRemainder(ItemStack stack) {
         return new ItemStack(this);
+    }
+
+    @Override
+    public Multimap<EntityAttribute, EntityAttributeModifier> getAttributeModifiers(ItemStack stack, EquipmentSlot slot) {
+        Multimap<EntityAttribute, EntityAttributeModifier> modifiers = super.getAttributeModifiers(stack, slot);
+
+        if (slot == EquipmentSlot.MAINHAND) {
+            ImmutableMultimap.Builder<EntityAttribute, EntityAttributeModifier> builder = ImmutableMultimap.builder();
+            builder.putAll(modifiers);
+
+            // Default is 3, so put putting 2 I add 2 more blocks
+            builder.put(ReachEntityAttributes.ATTACK_RANGE, new EntityAttributeModifier(
+                    ATTACK_RANGE_MODIFIER_UUID,
+                    "Weapon attack range bonus",
+                    2.0D,
+                    EntityAttributeModifier.Operation.ADDITION
+            ));
+
+            return builder.build();
+        }
+
+        return modifiers;
     }
 
     // Messages sent when you hit an entity
